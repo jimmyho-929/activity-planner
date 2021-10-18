82ffbeabd1d5e1a9562a37e3684eaf3e84c7908b
jimmy <jimmyho.929@outlook.com>
2021-10-18 00:49:23 -0400

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace activity_tracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!");
         }
     }
 }
